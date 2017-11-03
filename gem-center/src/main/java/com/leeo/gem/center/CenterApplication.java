a91f1713b724f731481d1229c8b353744ec475de
leeo <leeo1124@163.com>
2017-11-03 19:08:49 +0800

    init

---
+++ gem-center/src/main/java/com/leeo/gem/center/CenterApplication.java
@@ -0,0 +1,13 @@
+package com.leeo.gem.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableEurekaServer
+@SpringBootApplication
+public class CenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterApplication.class, args);
+	}
+}
