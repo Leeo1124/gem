0d73113783cc5f98edc199b51e16f08aea85ddf8
leeo <leeo1124@163.com>
2017-11-05 21:22:16 +0800

    添加zuul、eureka、feign、ribbon

--- gem-center/src/main/java/com/leeo/gem/center/CenterApplication.java
+++ gem-center/src/main/java/com/leeo/gem/center/CenterApplication.java
@@ -2,12 +2,17 @@ package com.leeo.gem.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//@EnableEurekaServer
+@EnableEurekaServer
 @SpringBootApplication
 public class CenterApplication {
 
 	public static void main(String[] args) {
+// java -jar gem-center.jar --spring.profiles.active=center1
+// java -jar gem-center.jar --spring.profiles.active=center2
+// java -jar gem-center.jar --spring.profiles.active=center3
+
 		SpringApplication.run(CenterApplication.class, args);
 	}
 }
